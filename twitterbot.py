37cf8389aae6f1562fba3cc8c4d7d139479cc7d3
amelia <amelia@entirelyamelia.com>
2017-06-25 11:59:53 -0400

    wait only an hour

--- twitterbot.py
+++ twitterbot.py
@@ -22,19 +22,13 @@ bot.read(source)
 # Determine the size of the tweet
 size = random.randint(5, 25)
 
-seedwords = [] #['ptsd', 'refine', 'mom', 'boss', 'work']
+# Tweet it
+bot.twitter_login(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
+bot.twitter_tweeting_start(days=0, hours=1, minutes=3, keywords=None)
 
-text = bot.generate_text(size, seedwords)
-print("tweetbot says:")
-print(text)
-#
-# # Tweet it
-# bot.twitter_login(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
-# bot.twitter_tweeting_start(days=0, hours=1, minutes=3, keywords=None)
-#
-# def wait():
-#     secsinweek = 7 * 24 * 60 * 60
-#     time.sleep(secsinweek)
-#     wait()
-#
-# wait()
+def wait():
+    secsinhour = 60 * 60
+    time.sleep(secsinhour)
+    wait()
+
+wait()
